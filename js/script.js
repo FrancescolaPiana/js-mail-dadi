bc983dac5350bbe4d7e88c9f59cb9202593dab53
Francescolapiana <Lushika1219@gmail.com>
2022-10-11 17:39:15 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+const numerogen = [];
+const numerodidadi = 2;
+
+for ( let i = 1; index <= numerodidadi; i++){
+    let numerorisultato = Math.floor(Math.random() * (6 - 1) ) + 1;
+    numerogen.push(numerorisultato);
+};
+console.log(numerogen);
