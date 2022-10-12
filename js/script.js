5ed54ef3d44f672e6a2a13546128316cd8a87632
Francescolapiana <Lushika1219@gmail.com>
2022-10-12 10:27:53 +0200

    s

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 
 const button = document.getElementById('tiraidadi')
-const computer = document.getElementById('computer')
-const umano = document.getElementById('umano')
-const vincitore = document.getElementById('vincitore')
 
 
 const numerodidadi = 1;
@@ -19,10 +16,6 @@ for ( let i = 1; i <= numerodidadi; i++){
 for ( let i = 1; i <= numerodidadi; i++){
     numerorisultato2 = Math.floor(Math.random() * (7 - 1) ) + 1;
 } 
-
-document.getElementById('d').innerHTML = numerorisultato ;
-document.getElementById('d2').innerHTML = numerorisultato2 ;
-
 for ( let i = 1; i <= numerodidadi; i++){
     numerorisultato3 = Math.floor(Math.random() * (7 - 1) ) + 1;
 } 
@@ -30,19 +23,23 @@ for ( let i = 1; i <= numerodidadi; i++){
     numerorisultato4 = Math.floor(Math.random() * (7 - 1) ) + 1;
 } 
 
+document.getElementById('d').innerHTML = numerorisultato ;
+document.getElementById('d2').innerHTML = numerorisultato2 ;
 document.getElementById('d3').innerHTML = numerorisultato3 ;
 document.getElementById('d4').innerHTML = numerorisultato4 ;
 
-console.log(numerorisultato);
-console.log(numerorisultato2);
-console.log(numerorisultato3);
-console.log(numerorisultato4);
+// console.log(numerorisultato);
+// console.log(numerorisultato2);
+// console.log(numerorisultato3);
+// console.log(numerorisultato4);
 
 let valorecomputer = (numerorisultato + numerorisultato2);
 console.log(valorecomputer);
+// calcolo computer
 
 let valoreumano = (numerorisultato3 + numerorisultato4);
 console.log(valoreumano);
+// calcolo umano
 
 if (valorecomputer > valoreumano) {
    document.getElementById('vincitore').innerHTML = 'Spiacente hai perso Riprova..';
@@ -53,34 +50,8 @@ else if (valorecomputer < valoreumano){
 else{
    document.getElementById('vincitore').innerHTML = 'Pareggio';
 }
-
+// calcolo chi ha vinto
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 button.addEventListener('click', fx)
